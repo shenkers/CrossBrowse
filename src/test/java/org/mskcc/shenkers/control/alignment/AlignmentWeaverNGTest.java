f35f9941bf1b5a87539f19728640240dd379e064
sol <sol.shenker@gmail.com>
2015-02-03 16:35:28 -0500

    updated tests for new methods

--- src/test/java/org/mskcc/shenkers/control/alignment/AlignmentWeaverNGTest.java
+++ src/test/java/org/mskcc/shenkers/control/alignment/AlignmentWeaverNGTest.java
@@ -5,17 +5,24 @@
  */
 package org.mskcc.shenkers.control.alignment;
 
+import htsjdk.samtools.util.Interval;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javafx.collections.FXCollections;
 import javafx.util.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import static org.mskcc.shenkers.control.alignment.AlignmentWeaver.add;
 import static org.mskcc.shenkers.control.alignment.AlignmentWeaver.printAli;
 import org.mskcc.shenkers.model.datatypes.Genome;
+import org.mskcc.shenkers.util.IntervalTools;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -28,7 +35,7 @@ import org.testng.annotations.Test;
  * @author sol
  */
 public class AlignmentWeaverNGTest {
-    
+
     public AlignmentWeaverNGTest() {
     }
 
@@ -48,14 +55,12 @@ public class AlignmentWeaverNGTest {
     public void tearDownMethod() throws Exception {
     }
 
-    
-
     /**
      * Test of main method, of class AlignmentWeaver.
      */
     @Test
     public void testMain() {
-           List<AlignmentWeaver.Pos> l1 = new ArrayList<AlignmentWeaver.Pos>();
+        List<AlignmentWeaver.Pos> l1 = new ArrayList<AlignmentWeaver.Pos>();
         for (int i = 0; i < 3; i++) {
             l1.add(new AlignmentWeaver.Pos(i, i));
         }
@@ -140,8 +145,7 @@ public class AlignmentWeaverNGTest {
         }
     }
 
-  
-
+    static Logger logger = LogManager.getLogger();
 
     /**
      * Test of weave method, of class AlignmentWeaver.
@@ -155,11 +159,142 @@ public class AlignmentWeaverNGTest {
         Genome g3 = new Genome("dm3", "fly");
         Genome g4 = new Genome("dya", "yak");
         Genome g5 = new Genome("rnor", "rat");
-        alignments.put(new Pair<>(g1,g2), null);
-        alignments.put(new Pair<>(g2,g3), null);
-        alignments.put(new Pair<>(g3,g4), null);
-        alignments.put(new Pair<>(g4,g5), null);
+        alignments.put(new Pair<>(g1, g2), null);
+        alignments.put(new Pair<>(g2, g3), null);
+        alignments.put(new Pair<>(g3, g4), null);
+        alignments.put(new Pair<>(g4, g5), null);
         AlignmentWeaver.weave(alignments);
     }
-    
+
+    @Test
+    public void testMapping() {
+        logger.info("test mapping");
+        List<Pair<Integer, Integer>> l1 = new ArrayList<>();
+        List<Pair<Integer, Integer>> l2 = new ArrayList<>();
+        List<Pair<Integer, Integer>> l3 = new ArrayList<>();
+        List<Pair<Integer, Integer>> l4 = new ArrayList<>();
+        List<Pair<Integer, Integer>> l5 = new ArrayList<>();
+
+        l1.add(new Pair<>(10, 20));
+        l1.add(new Pair<>(30, 36));
+
+        l2.add(new Pair<>(13, 23));
+        l2.add(new Pair<>(27, 33));
+
+        l3.add(new Pair<>(13, 23));
+        l3.add(new Pair<>(1, 7));
+
+        l4.add(new Pair<>(20, 22));
+        l5.add(new Pair<>(9, 11));
+
+        LocalAlignment f = new LocalAlignment("a", "b", false, l1, l2);
+        LocalAlignment r = new LocalAlignment("a", "c", true, l1, l3);
+        LocalAlignment x = new LocalAlignment("a", "c", false, l4, l5);
+
+        NucleotideMapping nmf = new NucleotideMapping(new Interval("a", 10, 36), new Interval("b", 13, 33));
+        nmf.add(f);
+        NucleotideMapping nmr = new NucleotideMapping(new Interval("a", 10, 36), new Interval("c", 1, 23));
+        nmr.add(r);
+        nmr.add(x);
+    }
+
+    @Test
+    public void testAdd() {
+
+        logger.info("testing ADD");
+
+        Genome g1 = new Genome("mm10", "mouse");
+        Genome g2 = new Genome("hg19", "human");
+        Genome g3 = new Genome("dm3", "fly");
+        Genome g4 = new Genome("dya", "yak");
+        Genome g5 = new Genome("rnor", "rat");
+
+        Interval i1 = new Interval("", 0, 2);
+        Interval i2 = new Interval("", 0, 1);
+        Interval i3 = new Interval("", 0, 2);
+        Interval i4 = new Interval("", 0, 2);
+        Interval i5 = new Interval("", 0, 2);
+
+        AlignmentWeaver weaver = null;
+        {
+
+            weaver = new AlignmentWeaver(g1, i1, false);
+            logger.info("w_order {}", weaver.order);
+        }
+        {
+            Interval fromInterval = i1;
+            Interval toInterval = i2;
+            Genome fromGenome = g1;
+            Genome toGenome = g2;
+            NucleotideMapping mapping = new NucleotideMapping(fromInterval, toInterval);
+            mapping.add(new LocalAlignment("", "", false,
+                    Arrays.asList(
+                            new Pair(0, 0),
+                            new Pair(2, 2)),
+                    Arrays.asList(
+                            new Pair(0, 0),
+                            new Pair(1, 1))));
+            weaver.add(toInterval, fromGenome, toGenome, mapping);
+        }
+        logger.info("w_order {}", weaver.order);
+
+        {
+            Interval fromInterval = i2;
+            Interval toInterval = i3;
+            Genome fromGenome = g2;
+            Genome toGenome = g3;
+            NucleotideMapping mapping = new NucleotideMapping(fromInterval, toInterval);
+            mapping.add(new LocalAlignment("", "", false,
+                    Arrays.asList(
+                            new Pair(0, 0),
+                            new Pair(1, 1)),
+                    Arrays.asList(
+                            new Pair(0, 0),
+                            new Pair(2, 2))));
+            weaver.add(toInterval, fromGenome, toGenome, mapping);
+        }
+        logger.info("w_order {}", weaver.order);
+
+        {
+            Interval fromInterval = i3;
+            Interval toInterval = i4;
+            Genome fromGenome = g3;
+            Genome toGenome = g4;
+            NucleotideMapping mapping = new NucleotideMapping(fromInterval, toInterval);
+            mapping.add(new LocalAlignment("", "", false,
+                    Arrays.asList(
+                            new Pair(0, 0)),
+                    Arrays.asList(
+                            new Pair(2, 2))));
+            weaver.add(toInterval, fromGenome, toGenome, mapping);
+        }
+        logger.info("w_order {}", weaver.order);
+
+        {
+            Interval fromInterval = i4;
+            Interval toInterval = i5;
+            Genome fromGenome = g4;
+            Genome toGenome = g5;
+            NucleotideMapping mapping = new NucleotideMapping(fromInterval, toInterval);
+            mapping.add(new LocalAlignment("", "", false,
+                    Arrays.asList(
+                            new Pair(2, 2)),
+                    Arrays.asList(
+                            new Pair(0, 0))));
+            weaver.add(toInterval, fromGenome, toGenome, mapping);
+        }
+        logger.info("w_order {}", weaver.order);
+        weaver.printAli2(Arrays.asList(g1,g2,g3,g4,g5));
+    }
+//        {
+//            List<Integer> newSeq = new ArrayList<Integer>();
+//            for (int i = 0; i < 3; i++) {
+//                newSeq.add(i);
+//            }
+//            Map<Integer, Integer> m = new HashMap<Integer, Integer>();
+//            m.put(2, 0);
+//
+//            AlignmentWeaver.Ali a = new AlignmentWeaver.Ali(m);
+//            add(ali, newSeq, 3, a);
+//        }
 }
--- src/test/java/org/mskcc/shenkers/control/alignment/ChainParserNGTest.java
+++ src/test/java/org/mskcc/shenkers/control/alignment/ChainParserNGTest.java
@@ -20,6 +20,7 @@ import java.util.zip.GZIPInputStream;
 import javafx.util.Pair;
 import org.apache.commons.lang3.StringUtils;
 import org.mskcc.shenkers.model.datatypes.GenomeSpan;
+import org.mskcc.shenkers.util.IntervalTools;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -52,36 +53,7 @@ public class ChainParserNGTest {
     public void tearDownMethod() throws Exception {
     }
 
-    /**
-     *
-     * @param s
-     * @param e
-     * @param start
-     * @param eend
-     * @return whether (s,e) is nested within (start,end)
-     */
-    public static boolean isContained(int s, int e, int start, int end) {
-        return s > start - 1 && e < end + 1 && s < e + 1;
-    }
-
-    /**
-     *
-     * @param s
-     * @param e
-     * @param start
-     * @param eend
-     * @return whether (s,e) is nested within (start,end)
-     */
-    public static boolean overlaps(int s, int e, int start, int end) {
-        return (s > start - 1 && s < end + 1) || (e > start - 1 && e < end + 1) || (start > s - 1 && end < e + 1);
-    }
-
-    public static int[] intersection(int s1, int e1, int s2, int e2) {
-        if (!overlaps(s1, e1, s2, e2)) {
-            throw new RuntimeException(String.format("intervals (%d,%d) and (%d,%d) do not overlap", s1, e1, s2, e2));
-        }
-        return new int[]{Math.max(s1, s2), Math.min(e1, e2)};
-    }
+    
 
     public static LocalAlignment trim(LocalAlignment blocks, Interval query_i, Interval target_i) {
 
@@ -93,11 +65,11 @@ public class ChainParserNGTest {
             Pair<Integer, Integer> fromBlock = blocks.fromBlocks.get(i);
             Pair<Integer, Integer> toBlock = blocks.toBlocks.get(i);
 
-            assert isContained(fromBlock.getKey(), fromBlock.getValue(), query_i.getStart(), query_i.getEnd()) : "it is assumed that all blocks in query will be contained in the query interval";
+            assert IntervalTools.isContained(fromBlock.getKey(), fromBlock.getValue(), query_i.getStart(), query_i.getEnd()) : "it is assumed that all blocks in query will be contained in the query interval";
 
             // if this block overlaps it is either OK as is, or needs to be trimmed
-            if (overlaps(toBlock.getKey(), toBlock.getValue(), target_i.getStart(), target_i.getEnd())) {
-                if (isContained(toBlock.getKey(), toBlock.getValue(), target_i.getStart(), target_i.getEnd())) {
+            if (IntervalTools.overlaps(toBlock.getKey(), toBlock.getValue(), target_i.getStart(), target_i.getEnd())) {
+                if (IntervalTools.isContained(toBlock.getKey(), toBlock.getValue(), target_i.getStart(), target_i.getEnd())) {
                     fromBlocks.add(fromBlock);
                     toBlocks.add(toBlock);
                 } else {
