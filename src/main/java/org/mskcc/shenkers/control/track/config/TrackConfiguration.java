3ef16fcd94185accaa067ac375512516fd5449e4
sol <sol.shenker@gmail.com>
2015-03-20 15:03:49 -0400

    support for bigwig and gtf files

--- src/main/java/org/mskcc/shenkers/control/track/config/TrackConfiguration.java
+++ src/main/java/org/mskcc/shenkers/control/track/config/TrackConfiguration.java
@@ -16,6 +16,10 @@ import org.mskcc.shenkers.control.track.TrackBuilderImpl;
 import org.mskcc.shenkers.control.track.TrackFactory;
 import org.mskcc.shenkers.control.track.TrackFactoryImpl;
 import org.mskcc.shenkers.control.track.View;
+import org.mskcc.shenkers.control.track.bigwig.BigWigContext;
+import org.mskcc.shenkers.control.track.bigwig.BigWigView;
+import org.mskcc.shenkers.control.track.gene.GeneModelContext;
+import org.mskcc.shenkers.control.track.gene.GeneModelView;
 
 /**
  *
@@ -29,14 +33,32 @@ public class TrackConfiguration extends AbstractModule {
         bind(TrackBuilder.class).to(TrackBuilderImpl.class);
         
         // For each data type, bind a single factory
+        // BAM
         bind(new TypeLiteral<TrackFactory<BamContext>>() {})
                 .to(new TypeLiteral<TrackFactoryImpl<BamContext>>() {});
+        // BIGWIG
+        bind(new TypeLiteral<TrackFactory<BigWigContext>>() {})
+                .to(new TypeLiteral<TrackFactoryImpl<BigWigContext>>() {});
+        
+        // GTF
+        bind(new TypeLiteral<TrackFactory<GeneModelContext>>() {})
+                .to(new TypeLiteral<TrackFactoryImpl<GeneModelContext>>() {});
         
         // For each data type, bind all available views to be injected into the factory
+        // BAM
         TypeLiteral<View<BamContext>> bamViewType = new TypeLiteral<View<BamContext>>() {};
         Multibinder<View<BamContext>> bamViews = Multibinder.newSetBinder(binder(), bamViewType);
-        bamViews.addBinding().to(BamView1.class);
+//        bamViews.addBinding().to(BamView1.class);
         bamViews.addBinding().to(BamView2.class);
+        //BIGWIG
+        TypeLiteral<View<BigWigContext>> bigWigViewType = new TypeLiteral<View<BigWigContext>>() {};
+        Multibinder<View<BigWigContext>> bigWigViews = Multibinder.newSetBinder(binder(), bigWigViewType);
+        bigWigViews.addBinding().to(BigWigView.class);
+        
+        //GTF
+        TypeLiteral<View<GeneModelContext>> gtfViewType = new TypeLiteral<View<GeneModelContext>>() {};
+        Multibinder<View<GeneModelContext>> gtfViews = Multibinder.newSetBinder(binder(), gtfViewType);
+        gtfViews.addBinding().to(GeneModelView.class);
     }
 
 }
